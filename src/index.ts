9051629995d2e662145b9a6cb74f0fbf5d16561a
rafiakhtarmaham <rafiaakhtarmaham@gmail.com>
2024-03-18 10:12:20 +0530

    type

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,35 @@
-let a = 25;
-let myname = "rafia"
+// let a = 25;
+// let myname = "rafia"
 
-console.log(a)
+// console.log(a)
 
-let b: string = "{maha}"
-let c : number = 999
-let d : boolean 
+// let b: string = "{maha}"
+// let c : number = 999
+// let d : boolean 
 
-let myfirstname : string | number
+// let myfirstname : string | number
 
-myfirstname = "34"
+// myfirstname = "34"
 
 
-const func = (req: number, res: number): number=>{
-    console.log(req, res)
-    // return  String (req*res)
-    return  (req*res)
+// const func = (req: number, res: number): number=>{
+//     console.log(req, res)
+//     // return  String (req*res)
+//     return  (req*res)
+// }
+
+
+//type Alias
+
+type userName = string
+type userName2 = string | number
+
+let a: userName = "maha"
+let b: userName2 = 25
+
+type useName3 = (n:number, m:number) => number;
+
+const func: useName3 = (n, m)=>{
+    console.log(n,m)
+    return n * m
 }
