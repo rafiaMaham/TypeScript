3b40092232a45c641dd745848f87d30c6a902bad
rafiakhtarmaham <rafiaakhtarmaham@gmail.com>
2024-03-18 09:34:47 +0530

    basics

---
+++ dist/index.js
@@ -0,0 +1,14 @@
+"use strict";
+let a = 25;
+let myname = "rafia";
+console.log(a);
+let b = "{maha}";
+let c = 999;
+let d;
+let myfirstname;
+myfirstname = "34";
+const func = (req, res) => {
+    console.log(req, res);
+    // return  String (req*res)
+    return (req * res);
+};
---
+++ src/index.ts
@@ -0,0 +1,19 @@
+let a = 25;
+let myname = "rafia"
+
+console.log(a)
+
+let b: string = "{maha}"
+let c : number = 999
+let d : boolean 
+
+let myfirstname : string | number
+
+myfirstname = "34"
+
+
+const func = (req: number, res: number): number=>{
+    console.log(req, res)
+    // return  String (req*res)
+    return  (req*res)
+}
